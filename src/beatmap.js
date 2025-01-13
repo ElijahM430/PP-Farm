d693194948bad6b269d695ba2d517d04a2bc12fb
ElijahM430 <elijahmartin430@gmail.com>
2025-01-13 12:47:15 -0500

    Add files via upload
    Added additional purchasable buttons and payment requirement for buttons

--- src/beatmap.js
+++ src/beatmap.js
@@ -2,11 +2,12 @@ class beatmap {
     constructor(songName, baseCps, baseCost, buttonId) {
         this.songName = songName;
         this.baseCps = baseCps / (1000 / tickRate);
+        this.baseCost = baseCost;
         this.cost = baseCost;
         this.buttonId = buttonId;
         this.amountOwned = 0;
         this.cps = 0;
-        this.visible = true; // change this to false!!!!!!
+        this.visible = false; // change this to false!!!!!!
     }
 
     purchase(){
@@ -14,5 +15,26 @@ class beatmap {
         this.amountOwned++;
         this.cps = this.baseCps * this.amountOwned;
         this.cost = Math.ceil(this.cost * 1.15 ** this.amountOwned)
+    }
+
+    buttonState() {
+        if(!this.visible) {
+            document.getElementById(this.buttonId).style.display = "none";
+            if (score >= this.baseCost) {
+                this.visible = true;
+            document.getElementById(this.buttonId).style.display = 'initial';
+            }
+        }
+
+        if (score < this.cost) {
+            document.getElementById(this.buttonId).disabled = true;
+        } else {
+            document.getElementById(this.buttonId).disabled = false;
+        }
+
+        document.getElementById(this.buttonId).innerHTML = "Buy "  +
+        this.songName + " (cost: PP" + Math.ceil(this.cost).toLocaleString()
+        + ") <br> Adds " + (this.baseCps * (1000/ tickRate)).toLocaleString() 
+        + " PP per second <br> [Owned: " + this.amountOwned + "]";
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 const tickRate = 1000 / 30;
 let score = 0;
 
-let harumachi = new beatmap('harumachi', 0.1, 15, 'buyHarumachiClover');
+let Harumachi = new beatmap('harumachi', 0.1, 15, 'buyHarumachiClover');
+let inferno = new beatmap('inferno', 0.5, 50, 'buyInferno');
+let songthatmightplaywhenyoufightsans = new beatmap('songthatmightplaywhenyoufightsans', 5, 500, 'buySongthatmightplaywhenyoufightsans');
 
 function incScore() {
-    score += harumachi.cps;
+    score += Harumachi.cps;
+    score += inferno.cps;
+    score += songthatmightplaywhenyoufightsans.cps;
 }
 
 function scorePlusPlus(){
     score++;
 }
 
+function updateButtons() {
+    Harumachi.buttonState();
+    inferno.buttonState();
+    songthatmightplaywhenyoufightsans.buttonState();
+}
+
 function updatePage() {
     incScore();
+    updateButtons();
     document.getElementById('score').innerHTML = 
     Math.floor(score).toLocaleString();
 }
 
 setInterval(updatePage, tickRate);
-
