0daae38475da1a7b1d60cc463f9fe950316b1a78
ElijahM430 <elijahmartin430@gmail.com>
2025-01-28 13:01:15 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 const tickRate = 1000 / 30;
 clickStrength = 1;
-let score =1500;
+let score = 500;
 
 //Below are beatmaps
 let Harumachi = new beatmap('Harumachi Clover', 0.5, 15, 'buyHarumachiClover');
-let inferno = new beatmap('Inferno', 3, 150, 'buyInferno');
-let songthatmightplaywhenyoufightsans = new beatmap('Song that might play when you fight sans', 5, 500, 'buySongthatmightplaywhenyoufightsans');
-let backstabber = new beatmap('backstabber', 20, 1500, 'buyBackstabber');
+let inferno = new beatmap('Inferno', 3, 250, 'buyInferno');
+let songthatmightplaywhenyoufightsans = new beatmap('Song that might play when you fight sans', 5, 750, 'buySongthatmightplaywhenyoufightsans');
+let backstabber = new beatmap('backstabber', 20, 2500, 'buyBackstabber');
 
 // Below are upgrades
-let hidden1 = new hidden('Hidden for Harumachi Clover', 100, Harumachi, "buyHidden1");
-let hidden2 = new hidden('Hidden for Inferno', 500, inferno, "buyHidden2");
-let hidden3 = new hidden('Hidden for Sans', 1500, songthatmightplaywhenyoufightsans, "buyHidden3");
+let hidden1 = new hidden('Hidden for Harumachi Clover', 500, Harumachi, "buyHidden1");
+let hidden2 = new hidden('Hidden for Inferno', 1000, inferno, "buyHidden2");
+let hidden3 = new hidden('Hidden for Sans', 2000, songthatmightplaywhenyoufightsans, "buyHidden3");
 
 function scorePlusPlus() {
     score += clickStrength;
